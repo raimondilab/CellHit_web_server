8e8ee4ee9386e3acc1d88c685c5349c4c37ae905
nataliarosa9 <nataliarosa9@yahoo.com.br>
2024-03-27 20:22:49 +0100

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'integrar2_api',
+    name: 'cell_api',
     script: 'uvicorn',
     args: 'api:app --host 0.0.0.0 --port 8002',
     interpreter: 'python3',
