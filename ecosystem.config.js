780e74cb550a1446df34f6616865d5c391465949
nataliarosa9 <nataliarosa9@yahoo.com.br>
2024-04-09 11:10:03 +0200

    Function to check and reconnect to the database

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,9 @@ module.exports = {
     script: 'uvicorn',
     args: 'api:app --host 0.0.0.0 --port 8003',
     interpreter: 'python3',
-    exec_mode: 'fork'
+    exec_mode: 'fork',
+    env: {
+      CONDA_PREFIX: '/data/SW/anaconda3/envs/functional_database/'
+    }
   }]
 };
