ab6dbc7a07863f5b8dec98a5c56212cb3d905023
nataliarosa9 <nataliarosa9@yahoo.com.br>
2024-04-09 10:14:37 +0200

    header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -8,9 +8,7 @@ const Header = () => {
     <header className="gy-3">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3 backdrop" data-navbar-on-scroll="data-navbar-on-scroll">
         <div className="container">
-          <Link className="navbar-brand d-flex align-items-center fw-bolder fs-2 fst-italic" to="/">
-              <img src="/assets/images/logo.png" alt="CellHit" className="img-fluid w-15"/>
-        </Link>
+          <h1 className="navbar-brand d-flex align-items-center fw-bolder fs-2 fst-italic">CellHit</h1>
           <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
           <div className="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto pt-2 pt-lg-0">
