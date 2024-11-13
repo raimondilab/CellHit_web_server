bebf1c4941118ed3ce9741af8244fac4c90d8b12
nataliarosa9 <nataliarosa9@yahoo.com.br>
2024-11-13 16:03:44 +0100

    New Home Page

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -2,172 +2,171 @@ import React, { useState } from "react";
 import { Helmet } from 'react-helmet';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button } from 'primereact/button';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import { Card } from 'primereact/card';
+
 
 const HomePage = () => {
 
-const navigate = useNavigate();
-const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
-// Function to send form data to the API
-async function sendExploreData() {
+    // Function to send form data to the API
+    async function sendExploreData() {
         try {
-
-        const query = {
-              query: `
-               query getData{
-                    databases {
-                      __typename
-                       ... on Gdsc {
-                       gdscId
-                        drugName
-                      drugId
-                      source
-                      sampleIndex
-                      predictions
-                      predictionsStd
-                      quantileScore
-                      experimentalMin
-                      experimentalMedian
-                      experimentalMax
-                      modelMse
-                      modelCorr
-                      transcrCcleNeigh
-                      transcrCcleNeighCelllinename
-                      transcrCcleNeighOncotree
-                      responseCcleNeigh
-                      responseCcleNeighCelllinename
-                      responseCcleNeighOncotree
-                      transcrTcgaNeigh
-                      transcrTcgaNeighDiagnosis
-                      transcrTcgaNeighSite
-                      responseTcgaNeigh
-                      responseTcgaNeighDiagnosis
-                      responseTcgaNeighSite
-                      putativeTarget
-                      topLocalShapGenes
-                      recoveredTarget
-                      }
-                       ... on Prism {
-                         __typename
-                         prismId
-                         drugName
-                      drugId
-                      source
-                      sampleIndex
-                      predictions
-                      predictionsStd
-                      quantileScore
-                      experimentalMin
-                      experimentalMedian
-                      experimentalMax
-                      modelMse
-                      modelCorr
-                      transcrCcleNeigh
-                      transcrCcleNeighCelllinename
-                      transcrCcleNeighOncotree
-                      responseCcleNeigh
-                      responseCcleNeighCelllinename
-                      responseCcleNeighOncotree
-                      transcrTcgaNeigh
-                      transcrTcgaNeighDiagnosis
-                      transcrTcgaNeighSite
-                      responseTcgaNeigh
-                      responseTcgaNeighDiagnosis
-                      responseTcgaNeighSite
-                      putativeTarget
-                      topLocalShapGenes
-                      recoveredTarget
-                      }
+            const query = {
+                query: `
+                    query getData {
+                        databases {
+                            __typename
+                            ... on Gdsc {
+                                gdscId
+                                drugName
+                                drugId
+                                source
+                                sampleIndex
+                                predictions
+                                predictionsStd
+                                quantileScore
+                                experimentalMin
+                                experimentalMedian
+                                experimentalMax
+                                modelMse
+                                modelCorr
+                                transcrCcleNeigh
+                                transcrCcleNeighCelllinename
+                                transcrCcleNeighOncotree
+                                responseCcleNeigh
+                                responseCcleNeighCelllinename
+                                responseCcleNeighOncotree
+                                transcrTcgaNeigh
+                                transcrTcgaNeighDiagnosis
+                                transcrTcgaNeighSite
+                                responseTcgaNeigh
+                                responseTcgaNeighDiagnosis
+                                responseTcgaNeighSite
+                                putativeTarget
+                                topLocalShapGenes
+                                recoveredTarget
+                            }
+                            ... on Prism {
+                                __typename
+                                prismId
+                                drugName
+                                drugId
+                                source
+                                sampleIndex
+                                predictions
+                                predictionsStd
+                                quantileScore
+                                experimentalMin
+                                experimentalMedian
+                                experimentalMax
+                                modelMse
+                                modelCorr
+                                transcrCcleNeigh
+                                transcrCcleNeighCelllinename
+                                transcrCcleNeighOncotree
+                                responseCcleNeigh
+                                responseCcleNeighCelllinename
+                                responseCcleNeighOncotree
+                                transcrTcgaNeigh
+                                transcrTcgaNeighDiagnosis
+                                transcrTcgaNeighSite
+                                responseTcgaNeigh
+                                responseTcgaNeighDiagnosis
+                                responseTcgaNeighSite
+                                putativeTarget
+                                topLocalShapGenes
+                                recoveredTarget
+                            }
+                        }
                     }
-                    }
-              `
+                `
             };
 
-        let navigateData = null;
-        const apiUrl = 'https://api.cellhit.bioinfolab.sns.it/graphql';
+            let navigateData = null;
+            const apiUrl = 'https://api.cellhit.bioinfolab.sns.it/graphql';
 
-        navigateData = await axios.post(apiUrl, query);
+            navigateData = await axios.post(apiUrl, query);
 
-        if (!navigateData) {
+            if (!navigateData) {
                 setLoading(false);
                 Swal.fire({
                     icon: "info",
                     text: "No results found!"
-            });
-            return;
-        }else if (navigateData.data.errors){
-               setLoading(false);
+                });
+                return;
+            } else if (navigateData.data.errors) {
+                setLoading(false);
                 Swal.fire({
                     icon: "error",
                     text: "Oops... \n An error has occurred!"
                 });
-                 return;
-        }else if (navigateData) {
+                return;
+            } else if (navigateData) {
+                setLoading(false);
+                navigate("/explore/", { state: { 'data': navigateData.data } });
+            }
+        } catch (error) {
             setLoading(false);
-            navigate("/explore/", { state: { 'data': navigateData.data } });
-          }
+            Swal.fire({
+                icon: "error",
+                text: error.message
+            });
         }
+    }
 
-        catch (error) {
-         setLoading(false);
-          Swal.fire({
-              icon: "error",
-              text: error.message
-          });
-       }
-}
-
-const load = () => {
+    const load = () => {
         setLoading(true);
         sendExploreData();
-};
+    };
 
-  return (
-    <>
-      <Helmet>
-        <title>CellHit | Home</title>
-      </Helmet>
-      <Header />
-    <section className="py-1" id="home">
-    <div className="container">
-        <div className="row align-items-center min-vh-100 min-vh-md-100 vh-sm-100 vh-100">
-            <div className="col-sm-6 col-md-6 col-lg-6 text-sm-start text-center">
-                <h1 className="display-2 fw-semi-bold lh-sm fs-4 fs-lg-6 fs-xxl-8">Discover CellHit</h1>
-                <p className="mb-4 fs-1 fs-lg-1 fs-xxl-2">
-                   Interpretable models for drug-response prediction
-                </p>
-                 <Button  className="btn-home  shadow-none" label="Explore now" loading={loading} onClick={load} />
-                <div className="row align-items-center row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5 hide-mobile">
-                    <div className="col d-flex align-items-start vertical_line">
-                        <div>
-                            <h4 className="fw-bold mb-0">GDSC</h4>
-                            <p className="mb-0">887 Cell lines</p>
-                            <p>6337 Drugs</p>
-                        </div>
-                    </div>
-                    <div className="col d-flex align-items-start vertical_line">
-                        <div>
-                            <h4 className="fw-bold mb-0">PRISM</h4>
-                            <p className="mb-0">686 Cell lines</p>
-                            <p>286 Drugs</p>
+    return (
+        <>
+            <Helmet>
+                <title>CellHit | Home</title>
+            </Helmet>
+            <Header />
+            <section className="py-1" id="home">
+                <div className="container d-flex align-items-center justify-content-center min-vh-100 text-center">
+                    <div className="row align-items-center">
+                        <div className="col">
+                            <img src="/assets/images/1.svg" alt="Database" className="img-fluid w-75" />
+                            <h1 className="display-2 fw-semi-bold lh-sm fs-4 fs-lg-6 fs-xxl-8">Discover CellHit</h1>
+                            <p className="mb-4 fs-1 fs-lg-1 fs-xxl-2">
+                                Interpretable models for drug-response prediction
+                            </p>
+                            <div className="mb-4">
+                                <Button className="btn-home shadow-none me-3"  size="large" label="Explore now" loading={loading} onClick={load} />
+                                <Button className="btn-home-two shadow-none"  size="large" label="Run CellHit" />
+                            </div>
+                            <div className="row align-items-center hide-mobile">
+                                <div className="col">
+                                    <Card className="text-start">
+                                        <h4 className="fw-bold mb-1">GDSC</h4>
+                                        <p className="mb-1">887 Cell lines</p>
+                                        <p>6337 Drugs</p>
+                                    </Card>
+                                </div>
+                                <div className="col">
+                                    <Card className="text-start">
+                                        <h4 className="fw-bold mb-1">PRISM</h4>
+                                        <p className="mb-1">686 Cell lines</p>
+                                        <p>286 Drugs</p>
+                                    </Card>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-             <div className="col-sm-6 col-md-6 col-lg-6 text-sm-start text-center">
-               <img src="/assets/images/bg.png" alt="Database" className="img-fluid"/>
-             </div>
-        </div>
-    </div>
-</section>
-      <Footer/>
-    </>
-  );
+            </section>
+            <Footer />
+        </>
+    );
 };
 
 export default HomePage;
-
