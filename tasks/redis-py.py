bac4a205c5cb1ba6eac060534d04cdfed39e37a8
nataliarosa9 <nataliarosa9@yahoo.com.br>
2024-12-06 11:41:01 +0100

    Clean redis database

--- tasks/redis-py.py
+++ tasks/redis-py.py
@@ -1,8 +1,9 @@
 import redis
 
-# Connettersi al database numero 1
-r = redis.Redis(host='localhost', port=6379, db=1)
+# Connettersi al database Redis numero 0
+r = redis.Redis(host='localhost', port=6379, db=0)
 
-# Impostare e leggere valori
-r.set('chiave', 'valore')
-print(r.get('chiave'))  # Output: b'valore'
+# Eliminare tutte le chiavi dal database
+r.flushdb()
+
+print("Tutte le chiavi sono state eliminate dal database 0.")
