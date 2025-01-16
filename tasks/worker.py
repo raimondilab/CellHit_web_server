9c5833aaaafef12bdbba48c7105c91d1befa07c8
nataliarosa9 <nataliarosa9@yahoo.com.br>
2025-01-16 10:35:58 +0100

    Fix BackendStoreError and improve merge

--- tasks/worker.py
+++ tasks/worker.py
@@ -32,8 +32,8 @@ BASE_DIR = Path(__file__).resolve().parent
 PARENT_DIR = BASE_DIR.parent
 
 celery = Celery(__name__)
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
+celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/1")
+celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
 
 celery.conf.update(
     task_serializer='json',
