d89fe47eb34fea179eb04e98e6326c0b319bb869
nataliarosa9 <nataliarosa9@yahoo.com.br>
2025-01-20 09:51:46 +0100

    Fix bar color heatmap position

--- tasks/worker.py
+++ tasks/worker.py
@@ -326,8 +326,11 @@ def draw_heatmap(heatmap_df):
     height = len(heatmap_df) * 20 if len(heatmap_df) * 20 >= 500 else 500
     width = len(processed_data) * 10 if len(processed_data) * 10 >= 500 else 500
 
+    # Find the length of the longest column name
+    max_col_name_length = max(len(col) for col in heatmap_df.columns)
+
     # Generate heatmap using pt.clustergram (assuming pt is a valid library here)
-    return pt.clustergram(processed_data, height=height, width=width, xpad=100), height
+    return pt.clustergram(processed_data, height=height, width=width, xpad=100 + max_col_name_length), height
 
 
 # Preprocess 'ShapDictionary' to replace `np.float32(...)` with plain float values
