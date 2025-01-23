587d2a180dd64495623e0f4f5485200f9c02efdf
nataliarosa9 <nataliarosa9@yahoo.com.br>
2025-01-23 14:23:59 +0100

    Remove columns based on the mean of the standard deviations

--- tasks/worker.py
+++ tasks/worker.py
@@ -354,18 +354,18 @@ def draw_heatmap(heatmap_df, dataset):
     # Calculate standard deviation for each column
     std_devs = numeric_data.std()
 
-    # Define a threshold for low variability (optional, or keep the top N columns)
-    low_variability_threshold = std_devs.quantile(0.25)  # Example: bottom 25% variability
+    # Calculate the threshold as the mean of the standard deviations
+    threshold = std_devs.mean()
 
     # Identify columns to remove based on low variability
-    columns_to_remove = std_devs[std_devs <= low_variability_threshold].index
+    columns_to_remove = std_devs[std_devs <= threshold].index
 
     # Drop these columns from the original dataframe
     processed_data = heatmap_df.drop(columns=columns_to_remove)
 
     # Calculate dimensions for the heatmap
     height = len(heatmap_df) * 20 if len(heatmap_df) * 20 >= 500 else 500
-    width = len(processed_data) * 15 if len(processed_data) * 10 >= 500 else 1000
+    width = len(processed_data) * 10 + 200 if len(processed_data) * 10 <= 500 else 500
 
     # Find the length of the longest column name
     max_col_name_length = max(len(col) for col in heatmap_df.columns) + 200
@@ -378,7 +378,6 @@ def draw_heatmap(heatmap_df, dataset):
 
     # Generate heatmap using pt.clustergram (assuming pt is a valid library here)
     return pt.clustergram(processed_data, height=height, width=width, xpad=xpad, color_bar_title=color_bar_title), height
-
 
 
 # Preprocess 'ShapDictionary' to replace `np.float32(...)` with plain float values
