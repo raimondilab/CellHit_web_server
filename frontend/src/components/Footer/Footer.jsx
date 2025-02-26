d649f0ff6aefe8a5f353051e99a28612c1596492
nataliarosa9 <nataliarosa9@yahoo.com.br>
2025-02-26 14:52:00 +0100

    footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-<>
-<footer className="footer bg-primary d-flex justify-content-center align-items-center vh-10">
-  <div className="container text-center">
-    <p className="fs--1 my-2 fw-bold text-200"> Disclaimer This resource is intended for research purposes and it must not substitute a doctor's medical judgement or healthcare professional advice</p>
-    <p className="fs--1 my-2 fw-bold text-200">
-      All rights Reserved &copy; Bioinformatics group of the BIO@SNS, Scuola Normale Superiore, Pisa (Italy) - 2024
-    </p>
-  </div>
-</footer>
+    <>
+      <footer className="footer bg-primary d-flex justify-content-center align-items-center vh-10">
+        <div className="container text-center d-flex flex-column justify-content-center">
+          <p className="fs--1 fw-bold text-200 mb-1 mt-2">
+            Disclaimer: This resource is intended for research purposes and it must not substitute a doctor's medical judgement or healthcare professional advice
+          </p>
+          <p className="fs--1 fw-bold text-200 mb-1">
+            All rights Reserved &copy; Bioinformatics group of the BIO@SNS, Scuola Normale Superiore, Pisa (Italy) - 2024
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-</>
-  )
-}
-
-export default Footer
+export default Footer;
