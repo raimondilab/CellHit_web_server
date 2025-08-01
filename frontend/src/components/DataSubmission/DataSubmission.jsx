084ffeb370d46af99acf12f4fe976856ac1f49c9
nataliarosa9 <nataliarosa9@yahoo.com.br>
2025-08-01 17:30:17 +0200

    validated samples columns number

--- frontend/src/components/DataSubmission/DataSubmission.jsx
+++ frontend/src/components/DataSubmission/DataSubmission.jsx
@@ -404,6 +404,11 @@ function validateFile(fileContent) {
 
         const sampleColumns = validColumns.filter(col => !requiredColumns.includes(col));
 
+        if (sampleColumns.length < 2) {
+              reject(new Error(`At least two sample columns are required. Only found ${sampleColumns.length}.`));
+              return;
+            }
+
         const cleanedData = data.map(row => {
           const cleanedRow = {};
           for (let col of validColumns) {
