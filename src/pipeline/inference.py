83f35bda54d6513a4818049a46650a0c8cd3a673
nataliarosa9 <nataliarosa9@yahoo.com.br>
2026-04-11 14:53:17 +0200

    set limit_load

--- src/pipeline/inference.py
+++ src/pipeline/inference.py
@@ -271,7 +271,6 @@ def run_full_inference(
         drug_stats_path=drug_stats_path,
         drug_metadata_path=drug_metadata_path,
         models_path=models_path,
-        limit_load=50,
         **kwargs
     )
     # compute quantile scores
